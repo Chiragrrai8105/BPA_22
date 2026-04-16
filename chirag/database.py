7a029e75c8fee202e109ff73320615d4cea56047
Chirag <chiragrrai44@gmail.com>
2026-04-16 15:03:31 +0530

    new folder

---
+++ chirag/database.py
@@ -0,0 +1,46 @@
+DATABASE = {
+    "Staphylococcus aureus": {
+        "shape": "cocci",
+        "arrangement": "clusters",
+        "size": "small",
+        "texture": "smooth",
+        "gram": "positive",
+        "weight": 1.0
+    },
+
+    "Staphylococcus epidermidis": {
+        "shape": "cocci",
+        "arrangement": "clusters",
+        "size": "small",
+        "texture": "smooth",
+        "gram": "positive",
+        "weight": 0.95
+    },
+
+    "Staphylococcus saprophyticus": {
+        "shape": "cocci",
+        "arrangement": "clusters",
+        "size": "small",
+        "texture": "smooth",
+        "gram": "positive",
+        "weight": 0.90
+    },
+
+    "Micrococcus luteus": {
+        "shape": "cocci",
+        "arrangement": ["tetrads", "clusters"],
+        "size": "small",
+        "texture": "smooth",
+        "gram": "positive",
+        "weight": 0.80
+    },
+
+    "Streptococcus pyogenes": {
+        "shape": "cocci",
+        "arrangement": "chains",
+        "size": "small",
+        "texture": "smooth",
+        "gram": "positive",
+        "weight": 0.85
+    }
+}
